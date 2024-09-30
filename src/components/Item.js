262e0a75702cdea3d150e73a7a4f5a7993606e8e
vasylBu <burdai.vasyl@student.uzhnu.edu.ua>
2024-09-30 23:49:33 +0300

    a few updates

---
+++ src/components/Item.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Item extends Component {
+  render() {
+    return (
+      <div className='item'>
+        <img src={""} />
+        <h2> {this.props.item.title}</h2>
+        <p> {this.props.item.disc}</p>
+        <b> {this.props.item.price}$</b>
+        <div className='addToCart'>+</div>
+       </div>
+    )
+  }
+}
+
+export default Item

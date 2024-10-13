46a6a6dfe77aea3751729cd02b0a912c8f1d58f8
vasylBu <burdai.vasyl@student.uzhnu.edu.ua>
2024-10-13 14:26:31 +0300

    test pullrequest

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={""} />
+        <img src={this.props.item.imageSrc} alt={this.props.item.title || 'item image'} />
         <h2> {this.props.item.title}</h2>
         <p> {this.props.item.disc}</p>
         <b> {this.props.item.price}$</b>
         <div className='addToCart'>+</div>
-       </div>
-    )
+      </div>
+    );
   }
 }
 
-export default Item
+export default Item;

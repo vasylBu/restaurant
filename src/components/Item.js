16b88ed532fd9cb1325ee8743ade35b18d3ccf3d
vasylBu <burdai.vasyl@student.uzhnu.edu.ua>
2024-10-15 13:49:33 +0300

    dev branch

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={this.props.item.imageSrc} alt={this.props.item.title || 'item image'} />
+        <img src={"./img/" + this.props.item.img} />
         <h2> {this.props.item.title}</h2>
         <p> {this.props.item.disc}</p>
         <b> {this.props.item.price}$</b>

c770572cca60dd8b2da5535e40929f0f9b83557b
vasylBu <burdai.vasyl@student.uzhnu.edu.ua>
2024-10-15 15:24:38 +0300

    custom react app

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'
+import PropTypes from 'prop-types'; 
+import React, { Component } from 'react';
+
 
 export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={""} />
-        <h2> {this.props.item.title}</h2>
-        <p> {this.props.item.disc}</p>
-        <b> {this.props.item.price}$</b>
+        <img src={"./img/" + this.props.item.img} alt={this.props.item.title} /> 
+        <h2>{this.props.item.title}</h2>
+        <p>{this.props.item.disc}</p>
+        <b>{this.props.item.price}$</b>
         <div className='addToCart'>+</div>
-       </div>
-    )
+      </div>
+    );
   }
 }
 
-export default Item
+Item.propTypes = {
+  item: PropTypes.shape({
+    img: PropTypes.string.isRequired,  
+    title: PropTypes.string.isRequired, 
+    disc: PropTypes.string.isRequired,   
+    price: PropTypes.string.isRequired,  
+  }).isRequired, 
+};
+
+export default Item;
